7d768e39ae295e6fb19d2df2998948ab1445d4af
Lipan Ioan <lipanioan420@gmail.com>
2023-10-13 13:01:53 +0300

    this is the application

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(-1);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setCount((prevCount) => prevCount + 1);
+    setAdvice(data.slip.advice);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <h1 className="advice-text">{advice}</h1>
+      <button className="advice-button" onClick={getAdvice}>
+        Get Advice
+      </button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p className="message-text">
+      You have read <strong>{props.count}</strong> advices!
+    </p>
+  );
+}
